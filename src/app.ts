5ba0b5e7b875c75c9945e19eeaa0e7f043e7c211
abr-ya <gamer05@list.ru>
2022-02-18 20:43:05 +0300

    Add simple logger service

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from "express";
 import { Server } from "http";
 import { userRouter } from "./users/users";
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
     app: Express;
     server: Server;
     port: number;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     useRoutes() {
@@ -19,7 +22,7 @@ export class App {
     public async init() {
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Сервер запущен на ${this.port}`);
+        this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
     }
 }
 
